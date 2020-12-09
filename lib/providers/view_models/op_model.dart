da9c1398e1b63b6147893c88ac905f6a6e2f4081
iota9star <iota9star@foxmail.com>
2020-12-10 00:17:19 +0800

    :bug: fixed subscribe not working.

--- lib/providers/view_models/op_model.dart
+++ lib/providers/view_models/op_model.dart
@@ -1,10 +1,25 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:mikan_flutter/internal/extension.dart';
 import 'package:mikan_flutter/internal/http.dart';
 import 'package:mikan_flutter/internal/repo.dart';
 import 'package:mikan_flutter/providers/view_models/base_model.dart';
 
 class OpModel extends CancelableBaseModel {
+  String _tapFlag;
+
+  String get tapFlag => _tapFlag;
+
+  set tapFlag(String value) {
+    _tapFlag = value;
+    notifyListeners();
+  }
+
+  performTap(final String flag) async {
+    this._tapFlag = flag;
+    Future.delayed(Duration(milliseconds: 640), () => this._tapFlag = null);
+  }
+
   subscribeBangumi(
     final String bangumiId,
     final bool subscribed, {
@@ -12,11 +27,25 @@ class OpModel extends CancelableBaseModel {
     final VoidCallback onSuccess,
     final ValueChanged<String> onError,
   }) async {
+    if (bangumiId.isNullOrBlank) {
+      return "番组id为空，忽略当前订阅".toast();
+    }
+    final int bid = int.tryParse(bangumiId);
+    if (bid == null) {
+      return "番组id为空，忽略当前订阅".toast();
+    }
+    int sid;
+    if (subgroupId.isNotBlank) {
+      sid = int.tryParse(subgroupId);
+      if (sid == null) {
+        return "字幕组id解析错误，忽略当前订阅".toast();
+      }
+    }
     final Resp resp = await (this +
         Repo.subscribeBangumi(
-          bangumiId,
+          bid,
           subscribed,
-          subgroupId: subgroupId,
+          subgroupId: sid,
         ));
     if (resp.success) {
       onSuccess?.call();
