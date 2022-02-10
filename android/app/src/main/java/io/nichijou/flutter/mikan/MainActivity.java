f0d2ede8dc218435244a84ccc38e2ed3f74a375a
iota9star <iota9star@foxmail.com>
2022-02-10 23:12:37 +0800

    :arrow_up: Upgrade Android dependencies.

--- android/app/src/main/java/io/nichijou/flutter/mikan/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package io.nichijou.flutter.mikan;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/io/nichijou/flutter/mikan_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.nichijou.flutter.mikan_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
