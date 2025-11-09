284145863d7c6f19b8bca0991b8956d2d556cbeb
iota9star <iota9star@foxmail.com>
2025-11-09 13:56:17 +0800

    🔧 Refactor bottom sheet widget to simplify layout and improve readability

--- lib/widget/bottom_sheet.dart
+++ lib/widget/bottom_sheet.dart
@@ -38,31 +38,17 @@ class MBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final clipRRect = ClipRRect(
-      borderRadius: borderRadius28,
+      borderRadius: borderRadiusT28,
       child: child,
     );
-    return Material(
-      color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.12),
-      borderRadius: borderRadiusT28,
-      child: Padding(
-        padding: EdgeInsets.only(
-          left: 16.0,
-          right: 16.0,
-          bottom: 16.0 + navKey.currentContext!.navBarHeight,
-          top: heightFactor == 1.0
-              ? navKey.currentContext!.statusBarHeight + 16.0
-              : 16.0,
-        ),
-        child: height != null
-            ? SizedBox(
-                height: height,
-                child: clipRRect,
-              )
-            : FractionallySizedBox(
-                heightFactor: heightFactor,
-                child: clipRRect,
-              ),
-      ),
-    );
+    return height != null
+        ? SizedBox(
+            height: height,
+            child: clipRRect,
+          )
+        : FractionallySizedBox(
+            heightFactor: heightFactor,
+            child: clipRRect,
+          );
   }
 }
